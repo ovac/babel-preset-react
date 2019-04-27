d60bd50e394afe3a69716dcbe9d6a10660459c15
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2019-04-27 13:48:49 +0000

    💫  my reusable babel preset

---
+++ README.md
@@ -0,0 +1,3 @@
+# babel-preset-react
+
+Collection of plugin and preset for compiling react assets as a package outside create-react-app or using laravel mix.
---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+	return {
+		presets: [
+			'@babel/preset-react',
+			['@babel/preset-env', { targets: { browsers: ['last 2 versions'] }, loose: true, modules: false }],
+			'@babel/preset-flow'
+		],
+		plugins: [
+			['@babel/plugin-proposal-decorators', { legacy: true }],
+			'@babel/plugin-proposal-class-properties',
+			'@babel/plugin-proposal-optional-chaining',
+			'@babel/plugin-proposal-object-rest-spread',
+			'@babel/plugin-syntax-dynamic-import',
+			'@babel/plugin-proposal-export-namespace-from',
+			'@babel/plugin-proposal-export-default-from',
+			['module-resolver', { alias: {} }]
+		]
+	};
+};
